63b1b9e69d05a984437c7e4837411e2e5da70ead
vbc221 <vbc221@nyu.edu>
2019-07-15 10:10:22 -0500

    finished questions

---
+++ Questions.js
@@ -0,0 +1,124 @@
+'use strict'
+
+const STORE= [
+  {
+    question: 'What is the capital of Japan?',
+    answers: [
+      'Osaka',
+      'Kyoto',
+      'Fukuoka',
+      'Tokyo'
+    ],
+  correctAnswer: 'Tokyo',
+    icon: 'https://www.google.com/search?q=japan+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjjnfvlkbfjAhWEX80KHbp6BY8Q_AUIECgB&biw=1119&bih=618#imgrc=diLipGTk7RAccM:',
+    alt: 'Japanese flag'
+  },
+  {
+    question: 'What is the capital of Japan?',
+    answers: [
+      'Hsinchu',
+      'Taipei',
+      'Kaohsiung',
+      'Tainan'
+    ],
+  correctAnswer: 'Taipei',
+    icon: 'https://www.google.com/search?q=taiwan+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiExrjgkbfjAhVKB50JHbztBQ0Q_AUIECgB&biw=1119&bih=618#imgrc=xA4B0bEO-JaNRM:',
+    alt: 'Taiwan flag'
+  },
+  {
+    question: 'What is the capital of China?',
+    answers: [
+      'Beijing',
+      'Shanghai',
+      'Shenzhen',
+      'Nanjing'
+    ],
+    correctAnswer: "Beijing",
+    icon: 'https://www.google.com/search?q=china+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiQ99DWkbfjAhUXOs0KHTFzAlMQ_AUIECgB&biw=1119&bih=618#imgrc=GeCanEo9fwa3RM:',
+    alt: 'China flag'
+  },
+  {
+    question: 'What is the capital of South Korea?',
+    answers: [
+      'Busan',
+      'Incheon',
+      'Seoul',
+      'Gimpo',
+    ],
+    correctAnswer: 'Seoul',
+    icon: 'https://www.google.com/search?q=south+korea+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjxsvbJkbfjAhVJVc0KHSlfD_oQ_AUIECgB&biw=1119&bih=618#imgrc=0qMmZI1yU3orzM:',
+    alt: 'South Korea flag'
+  },
+  {
+    question: 'What is the capital of Canada?',
+    answers: [
+      'Ottawa',
+      'Toronto',
+      'Montreal',
+      'Ontario'
+    ],
+    icon: 'https://www.google.com/search?q=canada+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj0q-_CkbfjAhUXHc0KHYzMBr0Q_AUIECgB&biw=1119&bih=618#imgrc=8BbVkRiS5q_auM:',
+    alt: 'Canda flag'
+  },
+  {
+    question: 'What is the capital of Australia?',
+    answers: [
+      'Canberra',
+      'Sydney',
+      'Melbourne',
+      'Perth',
+    ],
+    correctAnswer: 'Canberra',
+    icon: 'https://www.google.com/search?q=australia+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi3hJS6kbfjAhVFHc0KHZKLC-4Q_AUIECgB&biw=1119&bih=618#imgrc=gRsR_oXneAtsCM:',
+    alt: 'Australia Flag'
+  },
+  {
+    question: 'What is the capital of Mexico?',
+    answers: [
+      'Guadalajara',
+      'Puebla',
+      'Cancún',
+      'Mexico City'
+    ],
+    correctAnswer: 'Mexico City',
+    icon: 'https://www.google.com/search?q=mexico+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwio56uxkbfjAhXQZc0KHV08AGYQ_AUIECgB&biw=1119&bih=618#imgrc=s-kixUN_bpy9zM:',
+    alt: 'Mexico flag'
+  },
+  {
+    question: 'What is the capital of the United Kingdom?',
+    answers: [
+      'Liverpool',
+      'Bristol',
+      'London',
+      'Glasgow'
+    ],
+    correctAnswer: 'London',
+    icon: 'https://www.google.com/search?q=uk+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwimxZSpkbfjAhXaLc0KHcmcCocQ_AUIECgB&biw=1119&bih=618#imgrc=loDVUIgaAvTIPM:',
+    alt: 'United Kingdom Flag'
+  },
+  {
+    question: 'What is the capital of France?',
+    answers: [
+      'Nice',
+      'Paris',
+      'Lyon',
+      'Toulouse',
+    ],
+    correctAnswer: 'Paris',
+    icon: 'https://www.google.com/search?q=france+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwioipqbkbfjAhWVUs0KHRERDtAQ_AUIECgB&biw=1119&bih=618#imgrc=bAY4pm1ktOuMLM:',
+    alt: 'French Flag'
+  },
+  {
+    question: 'What is the capital of Italy?',
+    answers: [
+      'Florence',
+      'Venice',
+      'Milan',
+      'Rome'
+    ],
+    correctAnswer: 'Rome',
+    icon: 'https://www.google.com/search?q=italian+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi7r_2OkbfjAhWJG80KHcxMDsEQ_AUIECgB&biw=1119&bih=618#imgrc=hDjjQN_rnzImoM:',
+    alt: 'Italian flag'
+  }
+];
+
