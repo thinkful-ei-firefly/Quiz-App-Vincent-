a2da8cdd8a639cb0b20efcd168ecc607c77223c1
vbc221 <vbc221@nyu.edu>
2019-07-15 09:47:49 -0500

    question 4

--- script.js
+++ script.js
@@ -14,6 +14,38 @@ const STORE= [
     alt: 'Japanese flag'
   },
   {
-    question: ''
+    question: 'What is the capital of Japan?',
+    answers: [
+      'Hsinchu',
+      'Taipei',
+      'Kaohsiung',
+      'Tainan'
+    ],
+  correctAnswer: 'Taipei',
+    icon: 'https://www.google.com/search?q=taiwan+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiExrjgkbfjAhVKB50JHbztBQ0Q_AUIECgB&biw=1119&bih=618#imgrc=xA4B0bEO-JaNRM:',
+    alt: 'Taiwan flag'
+  },
+  {
+    question: 'What is the capital of China?',
+    answers: [
+      'Beijing',
+      'Shanghai',
+      'Shenzhen',
+      'Nanjing'
+    ],
+    correctAnswer: "Beijing",
+    icon: 'https://www.google.com/search?q=china+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiQ99DWkbfjAhUXOs0KHTFzAlMQ_AUIECgB&biw=1119&bih=618#imgrc=GeCanEo9fwa3RM:',
+    alt: 'China flag'
+  },
+  {
+    question: 'What is the capital of South Korea?',
+    answers: [
+      'Busan',
+      'Incheon',
+      'Seoul',
+      'Gimpo',
+    ],
+    icon: 'https://www.google.com/search?q=south+korea+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjxsvbJkbfjAhVJVc0KHSlfD_oQ_AUIECgB&biw=1119&bih=618#imgrc=0qMmZI1yU3orzM:',
+    alt: 'South Korea flag'
   }
 ]
