862530581f5189bbdc2dc0d2bacd2a1b3b3f9563
vbc221 <vbc221@nyu.edu>
2019-07-15 09:42:20 -0500

    question #1

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const STORE= [
+  {
+    question: 'What is the capital of Japan?',
+    answers: [
+      'Osaka',
+      'Kyoto',
+      'Fukuoka',
+      'Tokyo'
+    ],
+  correctAnswer: 'Tokyo',
+    icon: 'https://www.google.com/search?q=japan+flag&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjjnfvlkbfjAhWEX80KHbp6BY8Q_AUIECgB&biw=1119&bih=618#imgrc=diLipGTk7RAccM:',
+    alt: 'Japanese flag'
+  },
+  {
+    question: ''
+  }
+]
